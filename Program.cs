21811fbf19cf5c626b4d61378c8a488ff03a7d5f
Dmytro Viiuk <dmytro.viyuk@gmail.com>
2024-12-07 04:21:20 +0200

    another one

--- Program.cs
+++ Program.cs
@@ -1,18 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LabWork
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
+    public interface IBuilda
+    {
+        void AddSubject(string subject);
+        void SetDuration(int weeks);
+        void SetComplexityLevel(string complexity);
+        EducationalProgram Build();
+    }
+    public class Builda : IBuilda
+    {
+        private readonly EducationalProgram _program = new EducationalProgram();
+
+        public void AddSubject(string subject)
+        {
+            _program.Subjects.Add(subject);
+        }
+
+        public void SetDuration(int weeks)
+        {
+            _program.DurationInWeeks = weeks;
+        }
+
+        public void SetComplexityLevel(string complexity)
+        {
+            _program.ComplexityLevel = complexity;
+        }
+
+        public EducationalProgram Build()
+        {
+            return _program;
+        }
+    }
+    public class EducationalProgram
+    {
+        public List<string> Subjects { get; set; } = new List<string>();
+        public int DurationInWeeks { get; set; }
+        public string ComplexityLevel { get; set; }
+
+        public override string ToString()
+        {
+            return $"Educational Program:\n" +
+                   $"Subjects: {string.Join(", ", Subjects)}\n" +
+                   $"Duration: {DurationInWeeks} weeks\n" +
+                   $"Complexity Level: {ComplexityLevel}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World!");
+            Builda builder = new Builda();
+            Console.WriteLine("Add subjects, enter . to move on.");
+            bool complete = false;
+            string a;
+            while (!complete) {
+                a = Console.ReadLine();
+                if (a == ".") { complete = true; }
+                else { builder.AddSubject(a); }
+
+            }
+            Console.WriteLine("Enter duration in weeks");
+            builder.SetDuration(System.Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Enter complexity as a string");
+            builder.SetComplexityLevel(Console.ReadLine());
+            var example = builder.Build();
+            Console.WriteLine(example);
+
         }
     }
 }
